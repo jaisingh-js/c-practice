51807b01f26d3a3296699b8a77275b652ea9bea6
root <root@localhost.localdomain>
2022-02-20 20:51:23 +0530

    did ex1-10 exercise

--- book-exercises/chapter1/ex1-10.c
+++ book-exercises/chapter1/ex1-10.c
@@ -7,9 +7,21 @@ main()
 	int c;
 
 	while((c = getchar()) != EOF){
+		if(c!='\b' && c!='\t' && c!='\\'){
+			putchar(c);
+		}
+
 		if( c == '\t'){
 			putchar('\\');
 			putchar('t');
+		}
+		if(c=='\b'){
+			putchar('\\');
+			putchar('b');
+		}
+		if(c=='\\'){
+			putchar('\\');
+			putchar('\\');
 		}
 
 	}
